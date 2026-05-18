b55468a9c68e48cfcbc37e54a273455ee3948d98
blyedehumme-cmd <blyedehumme@gmail.com>
2026-05-18 10:57:11 -0400

    Update btc_bot.py

--- btc_bot.py
+++ btc_bot.py
@@ -540,10 +540,10 @@ def analyze_market(
 
         score = 0.0
         reasons = []
-        for ok, points, reason in checks:
+    for ok, points, reason in checks:
         if ok:
-        score += points
-        reasons.append(reason)
+            score += points
+            reasons.append(reason)
 
     signal = "BUY" if score >= MIN_CONFIDENCE else "WAIT"
 
